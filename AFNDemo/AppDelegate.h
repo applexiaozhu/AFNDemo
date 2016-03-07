d992ca8b11eff2359314a7c61b61f8ffd2d73221
iOSXiaoQiao <2996861140@qq.com>
2016-03-07 14:48:57 +0800

    Initial Commit

---
+++ AFNDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNDemo
+//
+//  Created by xiyang on 16/3/7.
+//  Copyright © 2016年 xiyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNDemo
+//
+//  Created by xiyang on 16/3/7.
+//  Copyright © 2016年 xiyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
